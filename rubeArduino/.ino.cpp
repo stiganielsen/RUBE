304713ba6c8a64da371a236e44bc5e31b3a3be18
Mikael Tulldahl <mikael.tulldahl@cpacsystems.se>
2016-03-08 03:19:11 +0100

    change folder and project name to rubeArduino to work with arduino IDE

--- rubeArduino/.ino.cpp
+++ rubeArduino/.ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-03-08 02:59:15
+//This file has been generated on 2016-03-08 03:15:59
 
 #include "Arduino.h"
 #include "Arduino.h"
@@ -35,7 +35,7 @@ void soundStart(void);
 void soundFinished(void);
 void soundError(void);
 
-#include "rube.ino"
+#include "rubeArduino.ino"
 
 #include "buttons.ino"
 #include "communication.ino"
